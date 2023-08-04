6bf1b28d7ec22d68291e727eb42d07cdcda5152f
DeniseSimone <denisesimone1997@gmail.com>
2023-08-04 09:51:20 +0200

    ...

---
+++ Files/JavaScript/ECommerce.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+
+/* Show or hide navbar when click hamburger menu */
+if (bar) { /* if nabvar is clicked */
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');   
+  })
+}
+if (close) { /* if nabvar is clicked */
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');   
+  })
+}
